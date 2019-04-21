3f92128d7603e54244558827a01264fd6445fc3e
José Proença <joseantonioproenca@gmail.com>
2019-04-21 20:17:09 +0100

    Automated the switch data generation.

--- src/layouts/hat-city-layout/switches.ts
+++ src/layouts/hat-city-layout/switches.ts
@@ -1,25 +1,14 @@
 import { SimpleMap } from '@logic/models/base';
 import { Switch, SwitchPosition } from '@logic/models/layout-descriptor/switch';
 
-export const switches: SimpleMap<Switch> = {
-  0: {
-    id: 0,
-    position: SwitchPosition.Straight,
-  } as Switch,
-  1: {
-    id: 1,
-    position: SwitchPosition.Straight,
-  } as Switch,
-  2: {
-    id: 2,
-    position: SwitchPosition.Straight,
-  } as Switch,
-  3: {
-    id: 3,
-    position: SwitchPosition.Straight,
-  } as Switch,
-  7: {
-    id: 7,
-    position: SwitchPosition.Turnout,
-  } as Switch,
-};
+export const switches: SimpleMap<Switch> = new Array(8).fill(null)
+.reduce(
+  (acc, val, idx) => ({
+    ...acc,
+    [idx]: {
+      id: idx,
+      position: SwitchPosition.Turnout,
+    } as Switch,
+  }),
+  {},
+);
