a6ca22176e5ab7ccd7fd83f62ae489ef319ceb01
José Proença <joseantonioproenca@gmail.com>
2019-04-22 19:11:38 +0100

    Added toggle on click to switches.

--- src/ble-train-control-panel/src/components/layout-view/layout-elem-switch-right.vue
+++ src/ble-train-control-panel/src/components/layout-view/layout-elem-switch-right.vue
@@ -1,16 +1,18 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg"
       v-if="data.position === switchPositionTurnout"
+      @click="toggleSwitch($event)"
       version="1.1"
       baseProfile="full"
       height="100%"
       width="100%">
       <line x1="0" y1="50%" x2="50%" y2="50%"></line>
-      <line x1="80%" y1="50%" x2="100%" y2="50%"></line>
+      <line x1="70%" y1="50%" x2="100%" y2="50%"></line>
       <line x1="50%" y1="50%" x2="100%" y2="150%"></line>
     </svg>
     <svg xmlns="http://www.w3.org/2000/svg"
       v-else-if="data.position === switchPositionStraight"
+      @click="toggleSwitch($event)"
       version="1.1"
       baseProfile="full"
       height="100%"
@@ -33,5 +35,9 @@
 
     private switchPositionStraight = SwitchPosition.Straight;
     private switchPositionTurnout = SwitchPosition.Turnout;
+
+    toggleSwitch($event) {
+      this.data.position = this.data.position === SwitchPosition.Straight ? SwitchPosition.Turnout : SwitchPosition.Straight;
+    }
   }
 </script>
