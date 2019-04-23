8eb88b238c9810c9418e4816257b8309dfc63a02
José Proença <joseantonioproenca@gmail.com>
2019-04-23 19:07:15 +0100

    Added state toggle by clicking to signal lights.

--- src/ble-train-control-panel/src/components/layout-view/layout-elem-signal-light.vue
+++ src/ble-train-control-panel/src/components/layout-view/layout-elem-signal-light.vue
@@ -1,15 +1,38 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg"
+    @click="toggleSwitch($event)"
     version="1.1"
     baseProfile="full"
     height="100%"
     width="100%">
     <line x1="10%" y1="5%" x2="40%" y2="5%" style="stroke-width: 15%"></line>
-    <rect x="40%" y="-20%" width="90%" height="50%" rx="20%" ry="20%" style="fill: white; stroke-width: 0;"></rect>
-    <circle v-if="data.state === stateGreen" cx="65%" cy="5%" r="20%" style="fill: darkred; stroke-width: 0;"></circle>
-    <circle v-if="data.state === stateGreen" cx="105%" cy="5%" r="20%" style="fill: lawngreen; stroke-width: 0;"></circle>
-    <circle v-if="data.state === stateRed" cx="65%" cy="5%" r="20%" style="fill: red; stroke-width: 0;"></circle>
-    <circle v-if="data.state === stateRed" cx="105%" cy="5%" r="20%" style="fill: darkgreen; stroke-width: 0;"></circle>
+    <rect x="40%"
+      y="-20%"
+      width="90%"
+      height="50%"
+      rx="20%"
+      ry="20%"
+      style="fill: white; stroke-width: 0;"></rect>
+    <circle v-if="data.state === stateGreen"
+      cx="65%"
+      cy="5%"
+      r="20%"
+      style="fill: darkred; stroke-width: 0;"></circle>
+    <circle v-if="data.state === stateGreen"
+      cx="105%"
+      cy="5%"
+      r="20%"
+      style="fill: lawngreen; stroke-width: 0;"></circle>
+    <circle v-if="data.state === stateRed"
+      cx="65%"
+      cy="5%"
+      r="20%"
+      style="fill: red; stroke-width: 0;"></circle>
+    <circle v-if="data.state === stateRed"
+      cx="105%"
+      cy="5%"
+      r="20%"
+      style="fill: darkgreen; stroke-width: 0;"></circle>
   </svg>
 </template>
 
@@ -22,9 +45,16 @@
 
   @Component({})
   export default class LayoutElemSignalLight extends Vue {
-    @Prop() private data!: SignalLight;
+    @Prop() data!: SignalLight;
 
-    private stateGreen = SignalLightState.Green;
-    private stateRed = SignalLightState.Red;
+    stateGreen = SignalLightState.Green;
+    stateRed = SignalLightState.Red;
+
+    toggleSwitch($event) {
+      this.data.state =
+        this.data.state === SignalLightState.Green ? SignalLightState.Red :
+          SignalLightState.Green;
+    }
+
   }
 </script>
