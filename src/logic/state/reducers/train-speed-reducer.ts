6c82ba2fb0ba6a06a27961357555569cd44b46eb
José Proença <joseantonioproenca@gmail.com>
2019-05-02 18:53:19 +0100

    Updated train speed reducer for handling "train.speedBeforeStop" field.

--- src/logic/state/reducers/train-speed-reducer.ts
+++ src/logic/state/reducers/train-speed-reducer.ts
@@ -1,9 +1,5 @@
 import { State } from '@logic/models/state';
-import {
-  ActionPayloadSwitch, ActionPayloadTrainSpeed,
-  ActionType,
-  LocalAction,
-} from '@logic/state/action';
+import { ActionPayloadTrainSpeed, ActionType, LocalAction } from '@logic/state/action';
 import { Reducer } from '@logic/state/store';
 
 export const trainSpeedReducer: Reducer<State, LocalAction<ActionPayloadTrainSpeed>> =
@@ -12,7 +8,11 @@ export const trainSpeedReducer: Reducer<State, LocalAction<ActionPayloadTrainSpe
       case ActionType.TrainSpeed: {
         const train = state.trains[action.payload.trainId];
         if (train) {
-          train.speed = action.payload.speed;
+          const newSpeed = action.payload.speed;
+          if (newSpeed === 0) {
+            train.speedBeforeStop = train.speed;
+          }
+          train.speed = newSpeed;
         }
       }
     }
